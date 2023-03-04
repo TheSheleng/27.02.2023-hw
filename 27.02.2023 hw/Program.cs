bbc928dddc80076821c05c23420b0992f9f89f11
TheSheleng <sheleng55@gmail.com>
2023-03-04 20:57:50 +0200

    Add project files.

---
+++ 27.02.2023 hw/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _27._02._2023_hw
+{
+    internal class Program
+    {
+        static void Swap<T>(ref T lhs, ref T rhs)
+        {
+            T temp;
+            temp = lhs;
+            lhs = rhs;
+            rhs = temp;
+        }
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
